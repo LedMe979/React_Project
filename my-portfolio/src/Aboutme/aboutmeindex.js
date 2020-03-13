a437a09662c7559fbba4bf521ec133c9e465e992
LedMe <ledinicmehmed@outlook.de>
2020-03-13 11:22:59 +0100

    create aboutme.jsom with text inside and create aboutmeindex.js

--- my-portfolio/src/Aboutme/aboutmeindex.js
+++ my-portfolio/src/Aboutme/aboutmeindex.js
@@ -4,11 +4,9 @@ import './App.css';
 class App extends Component {
 render (){   
   return (
-      <div className="conteinerEdit">
-
-      </div>
+        
     );
   }
 }
 
-export default App;
+export default aboutme;
